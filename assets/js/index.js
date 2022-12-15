f769ebeda96c7701de01bd53b311b63c54d6f973
Henry2061 <hongyu.henry.zhang@gmail.com>
2022-12-14 22:30:14 -0500

    index done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,54 @@
 $(function(){
+  getUserInfo()
+  $('#signout').on('click',signout)
+})
+// ss get username for welcome and avatar
+function getUserInfo(){
   $.ajax({
     url: '/my/userinfo',
     method: 'GET',
-    headers: {
-      Authorization: localStorage.getItem('token') || ''
-    },
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
     success: function(res){
-      console.log(localStorage.getItem('token'))
+      // console.log(localStorage.getItem('token'))
       // layui.layer.msg(res.message)
       if(res.status !==0) return console.log(res.message)
 
-      console.log(res)
+      // console.log(res)
+      let username=res.data.nickname || res.data.username
+      $('#welcome').html(`Welcome ${username} !`)
+      // console.log(username[0].toUpperCase())
 
+      // ss avatar conditional render
+      if(!res.data.user_pic) {
+        $('.layui-nav-img').hide()
+        $('.avatar').html(username[0].toUpperCase()).show()
+      } else {
+        $('.avatar').hide()
+        $('.layui-nav-img').prop('src',res.data.user_pic).show()
+      }
     }
+    
   })
-})
+}
+
+// ss signout
+function signout (){
+  // ss 弹出层=》confirm
+  layui.layer.confirm('Do you want to sign out?', {icon: 3, title:'Sign Out'}, function(index){
+    //do something
+    localStorage.removeItem('token')
+    location.href='/login.html'
+    // from layui
+    layer.close(index);
+  });
+  
+}
+
+/* vv ======================
+* layui.layer.confirm
+  layui is optional, layer.comfirm is good
+
+
+======================== */
--- assets/js/urlRoot.js
+++ assets/js/urlRoot.js
@@ -2,6 +2,19 @@ $.ajaxPrefilter(function(options){
   let urlRoot='http://www.liulongbin.top:3007'
   // console.log(options.url)
   options.url= urlRoot+options.url
+  // ss set headers for /my/
+  if(options.url.indexOf('/my/')!==-1 ) {
+    options.headers={
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  options.complete = function(res){
+    console.log(res.responseJSON)
+    if(res.responseJSON.status===1) {
+      localStorage.removeItem('token')
+      location.href='/login.html'
+    }
+  }
 })
 
 /* vv ==========================
